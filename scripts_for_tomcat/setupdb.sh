48ba1ccb736728756988d2d10b90721f08080964
Pavlo Kravets <60813108+Legamanchik@users.noreply.github.com>
2023-10-28 16:39:56 +0300

    Update setupdb.sh

--- scripts_for_tomcat/setupdb.sh
+++ scripts_for_tomcat/setupdb.sh
@@ -17,8 +17,5 @@ sudo -Hiu postgres createdb -O $POSTGRE_USER $DB_NAME
 echo "$DB_HOST:$DB_PORT:$DB_NAME:$POSTGRE_USER:$POSTGRE_PASSWORD" >> ~/.pgpass
 chmod 0600 ~/.pgpass
 
-# Copy the dump file to the PostgreSQL data directory
-sudo cp -f "$DUMP_FILE" "/var/lib/postgresql/$DUMP_FILE"
-
-# Restore the dump file to the PostgreSQL database
-sudo -Hiu postgres pg_restore -U $POSTGRE_USER -h $DB_HOST -d $DB_NAME -Fc "/var/lib/postgresql/$DUMP_FILE"
+# Restore the dump file using psql
+sudo -Hiu postgres psql -U $POSTGRE_USER -d $DB_NAME < "$DUMP_FILE"
