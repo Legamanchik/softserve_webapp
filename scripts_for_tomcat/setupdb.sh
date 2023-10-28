08512a53ec5208087ba2a24a13001237011d624e
Pavlo Kravets <60813108+Legamanchik@users.noreply.github.com>
2023-10-28 16:17:18 +0300

    Update setupdb.sh

--- scripts_for_tomcat/setupdb.sh
+++ scripts_for_tomcat/setupdb.sh
@@ -8,17 +8,17 @@ else
 fi
 
 # Create a PostgreSQL user
-sudo -u postgres psql -c "CREATE USER $POSTGRE_USER WITH PASSWORD '$POSTGRE_PASSWORD'"
+sudo -Hiu postgres psql -c "CREATE USER $POSTGRE_USER WITH PASSWORD '$POSTGRE_PASSWORD'"
 
 # Create a PostgreSQL database
-sudo -u postgres createdb -O $POSTGRE_USER $DB_NAME
+sudo -Hiu postgres createdb -O $POSTGRE_USER $DB_NAME
 
 # Set up the .pgpass file for password authentication
 echo "$DB_HOST:$DB_PORT:$DB_NAME:$POSTGRE_USER:$POSTGRE_PASSWORD" >> ~/.pgpass
 chmod 0600 ~/.pgpass
 
 # Copy the dump file to the PostgreSQL data directory
-cp "$DUMP_FILE" "/var/lib/postgresql/$DUMP_FILE"
+sudo cp -f "$DUMP_FILE" "/var/lib/postgresql/$DUMP_FILE"
 
 # Restore the dump file to the PostgreSQL database
-sudo -u postgres pg_restore -U $POSTGRE_USER -h $DB_HOST -d $DB_NAME -Fc "/var/lib/postgresql/$DUMP_FILE"
+sudo -Hiu postgres pg_restore -U $POSTGRE_USER -h $DB_HOST -d $DB_NAME -Fc "/var/lib/postgresql/$DUMP_FILE"
