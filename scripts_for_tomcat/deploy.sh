22183ab5be11a933f31f336fc1fc0fe8e0b26bff
Pavlo Kravets <60813108+Legamanchik@users.noreply.github.com>
2023-10-28 18:05:23 +0300

    Update deploy.sh

--- scripts_for_tomcat/deploy.sh
+++ scripts_for_tomcat/deploy.sh
@@ -2,14 +2,14 @@
 
 # Define your variables
 APP_DIR="/root/projects/java-app"
-SAPP_DIR="java-app" # or the name of the directory you want
+SAPP_DIR="ClassSchedule" # or the name of the directory you want
 TOMCAT_WEBAPPS_DIR="/var/lib/tomcat/webapps"
 
 # Create a directory for your app
 mkdir -p "$APP_DIR"
 
 # Clone your Java application repository
-git clone https://github.com/Legamanchik/softserve_webapp.git "$APP_DIR/$SAPP_DIR"
+git clone https://github.com/BlueTeam2/ClassSchedule "$APP_DIR/$SAPP_DIR"
 
 # Navigate to your app directory
 cd "$APP_DIR/$SAPP_DIR"
