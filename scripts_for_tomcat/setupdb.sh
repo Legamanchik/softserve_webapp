2df4e35dc9fc41864b0aedaf71ad1b1eae0938cb
Pavlo Kravets <60813108+Legamanchik@users.noreply.github.com>
2023-10-29 00:15:23 +0300

    Update setupdb.sh

--- scripts_for_tomcat/setupdb.sh
+++ scripts_for_tomcat/setupdb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if .env file exists
 if [ -f .env ]; then
   source .env
 else
@@ -15,21 +14,19 @@ sudo -u postgres psql -c "CREATE DATABASE schedule;" \
                       -c "GRANT ALL PRIVILEGES ON DATABASE schedule TO $POSTGRES_USERNAME;" \
                       -c "ALTER DATABASE schedule OWNER TO $POSTGRES_USERNAME;" \
                       -c "GRANT ALL PRIVILEGES ON DATABASE schedule_test TO $POSTGRES_USERNAME;" \
-                      -c "ALTER DATABASE schedule_test OWNER TO $POSTGRES_USERNAME"
+                      -c "ALTER DATABASE schedule_test OWNER TO $POSTGRES_USERNAME;"
 
 # Create .pgpass file
-PGPASSFILE="/var/lib/postgresql/.pgpass"
-echo "127.0.0.1:5432:*:$POSTGRES_USERNAME:$POSTGRES_PASSWORD" | sudo tee "$PGPASSFILE" > /dev/null
-sudo chmod 0600 "$PGPASSFILE"
-sudo chown postgres:postgres "$PGPASSFILE"
-
-# Add PGPASSFILE environment variable
-echo "export PGPASSFILE=$PGPASSFILE" | sudo tee -a /etc/environment > /dev/null
+sudo touch ".pgpass"
+echo "127.0.0.1:5432:*:$POSTGRES_USERNAME:$POSTGRES_PASSWORD" | sudo tee ".pgpass" > /dev/null
+sudo chmod 0600 ".pgpass"
+sudo chown postgres:postgres ".pgpass"
+echo PGPASSFILE=".pgpass" | sudo tee -a /etc/environment > /dev/null
 
 # Restore dump
-DUMP_FILE="2023-09-07.dump"
-sudo -u postgres psql --set ON_ERROR_STOP=off -d schedule -f "/tmp/init.dump"
-sudo -u postgres psql --set ON_ERROR_STOP=off -d schedule_test -f "/tmp/init.dump"
 
-# Restart PostgreSQL
+sudo cp 2023-09-07.dump /tmp/init.dump
+sudo -u postgres psql --set ON_ERROR_STOP=off -d schedule -f "/tmp/init.dump"
+sudo -u postgres psql --set ON_ERROR_STOP=off -d schedule -f "/tmp/init.dump"
+
 sudo systemctl restart postgresql
